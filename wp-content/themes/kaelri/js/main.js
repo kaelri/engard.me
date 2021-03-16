37d025c14021f87dbff36387a85f2541d0165a2e
Michael Engard <kaelri@gmail.com>
2021-03-16 16:53:58 -0400

    Fix Lightbox targeting.

--- wp-content/themes/kaelri/js/main.js
+++ wp-content/themes/kaelri/js/main.js
@@ -6,7 +6,7 @@ jQuery('.post_format-post-format-image, .type-project').each(function(){
 
 		var caption = jQuery(this).find('figcaption').html();
 
-		jQuery(this).find('a').attr( 'data-lightbox', id ).attr( 'data-title', caption );
+		jQuery(this).children('a').attr( 'data-lightbox', id ).attr( 'data-title', caption );
 		
 	});
 
